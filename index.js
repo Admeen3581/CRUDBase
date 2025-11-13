45cfd52295228c40dcaf77ce0ab48707695b8db8
Adam Long <adam.jacob.long@gmail.com>
2025-11-13 09:59:35 -0600

    express initialized

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Happy developing ✨')
+import express from 'express';
+
+const app = express();
+
+//Server Init on Start
+app.listen(3000, () =>
+{
+	console.log('Server started on port 3000!');
+});
+
+app.get("/", (req, res) => {
+	res.send("We are cooking");
+});
